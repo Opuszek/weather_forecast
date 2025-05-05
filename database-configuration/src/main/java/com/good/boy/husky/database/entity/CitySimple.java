0cecb05c010ee623a405346fb66f2d9fe063c51e
TomTheHusky <panopuszek@gmail.com>
2025-05-05 14:34:02 +0200

    move database connection to the module database-configuration (name to change)

---
+++ database-configuration/src/main/java/com/good/boy/husky/database/entity/CitySimple.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.good.boy.husky.database.entity;
+
+/**
+ *
+ * @author opuszek
+ */
+public class CitySimple {
+    private int id;
+    private String name;
+    private String country;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "CitySimple{" + "id=" + id + ", name=" 
+                + name + ", country=" + country + '}';
+    }
+
+    
+}
---
+++ database-configuration/src/main/java/com/good/boy/husky/database/service/DatabaseService.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.good.boy.husky.database.service;
+
+import com.good.boy.husky.database.configuration.DatabaseConfiguration;
+import com.good.boy.husky.database.entity.CitySimple;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author opuszek
+ */
+public class DatabaseService {
+
+    public List<CitySimple> getListOfUnlocatedCities() throws SQLException {
+        try (Connection con = getConnection()) {
+            return getListOfUnlocatedCities(con);
+        }
+    }
+
+    private List<CitySimple> getListOfUnlocatedCities(Connection con)
+            throws SQLException {
+        List<CitySimple> cities = new ArrayList<>();
+        try (Statement stmt = con.createStatement()) {
+            String selectSql = "SELECT id,name,country "
+                    + "FROM city "
+                    + "where located=0";
+            try (ResultSet resultSet = stmt.executeQuery(selectSql)) {
+                while (resultSet.next()) {
+                    CitySimple city = new CitySimple();
+                    city.setId(resultSet.getInt("id"));
+                    city.setName(resultSet.getString("name"));
+                    city.setCountry(resultSet.getString("country"));
+                    cities.add(city);
+                }
+            }
+        }
+        return cities;
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager
+                .getConnection(DatabaseConfiguration.getUrl(),
+                        DatabaseConfiguration.getUser(),
+                        DatabaseConfiguration.getPassword());
+    }
+
+}
