ef61492c4e2166a95e8c0a59c150862e225685f2
TomTheHusky <panopuszek@gmail.com>
2025-05-27 17:22:45 +0200

    Add weather-finder module

---
+++ weather-finder/src/main/java/com/good/boy/husky/weather/finder/WeatherFinder.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.good.boy.husky.weather.finder;
+
+/**
+ *
+ * @author opuszek
+ */
+public class WeatherFinder {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
