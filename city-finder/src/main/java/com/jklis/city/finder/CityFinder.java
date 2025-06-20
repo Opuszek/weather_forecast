96b7805e59471e0444c3663fd77c6577e007962d
Opuszek <panopuszek@gmail.com>
2025-06-20 22:03:33 +0200

    Add logging for cityFinder

--- city-finder/src/main/java/com/jklis/city/finder/CityFinder.java
+++ city-finder/src/main/java/com/jklis/city/finder/CityFinder.java
@@ -9,6 +9,8 @@ import io.vavr.control.Either;
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class CityFinder {
@@ -17,6 +19,8 @@ public class CityFinder {
     private static final GeolocalizeService geolocalizeService
             = new GeolocalizeService();
 
+    private static final Logger LOGGER = Logger.getLogger(CityFinder.class.getName());
+
     public static void main(String[] args) throws SQLException {
         List<CitySimple> unlocatedCities = databaseService.getListOfUnlocatedCities();
         List<Either<CityError, CityLocation>> results = unlocatedCities.stream()
@@ -24,6 +28,26 @@ public class CityFinder {
                 .collect(Collectors.toList());
         databaseService.updateCityLocations(getRightEithers(results));
         databaseService.logCityLocationErrors(getLeftEithers(results));
+        logResults(results);
+    }
+
+    private static void logResults(Collection<Either<CityError, CityLocation>> results) {
+        Collection<CityError> errs = getLeftEithers(results);
+        String logSuccess = String.format("Operation finished with %d succesful location(s)",
+                getRightEithers(results).size());
+        String logErrors = errs.isEmpty() ? ""
+                : String.format(" and %d following errors:%s%s", errs.size(),
+                        System.lineSeparator(), listErrors(errs));
+        LOGGER.log(Level.INFO, logSuccess + logErrors);
+    }
+    
+    private static String listErrors(Collection<CityError> locs) {
+        return locs.stream()
+                .collect(Collectors.toMap(CityError::getError, v -> 1L, Long::sum))
+                .entrySet().stream().map(
+                        e -> String.format("Exception %s occured %d times", 
+                                e.getKey(), e.getValue())
+                ).collect(Collectors.joining("," + System.lineSeparator()));
     }
 
     private static <T, Y> List<T> getLeftEithers(Collection<Either<T, Y>> eithers) {
